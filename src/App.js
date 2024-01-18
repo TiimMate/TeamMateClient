ef6a659ece79682ecd1668d33c700631bc996611
Seoyeon Park <qkrtj418@gmail.com>
2024-01-18 17:18:33 +0900

    Fix: Merge 충돌 해결

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Tapbar from './components/layouts/Tapbar';
 import Footer from './components/layouts/Footer';
 
 import Home from './pages/Home';
+import GuestApply from './pages/Matching.jsx/MatchApply/GuestApply';
+import TeamApply from './pages/Matching.jsx/MatchApply/TeamApply';
+import GuestHost from './pages/Matching.jsx/MatchHost/GuestHost';
+import TeamHost from './pages/Matching.jsx/MatchHost/TeamHost';
+import MatchingHome from './pages/Matching.jsx/MatchingHome';
 import Login from './pages/login/Login';
 import LoginLanding from './pages/login/LoginLanding';
 
@@ -30,6 +35,11 @@ function ContentBox() {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/welcome-user' element={<LoginLanding />} />
+        <Route path='/matching' element={<MatchingHome />} />
+        <Route path='/matching/guestapply' element={<GuestApply />} />
+        <Route path='/matching/teamapply' element={<TeamApply />} />
+        <Route path='/matching/guesthost' element={<GuestHost />} />
+        <Route path='/matching/teamhost' element={<TeamHost />} />
       </Routes>
       <Footer />
     </div>
