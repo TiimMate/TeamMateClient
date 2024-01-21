53ac358f452870e7ec9c760f08f630c14f53f6fa
Seoyeon Park <qkrtj418@gmail.com>
2024-01-21 12:38:21 +0900

    Feat: 대관정보 목록 페이지 레이아웃

---
+++ src/pages/location/LocationList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainFunctionNavbar from '../../components/layouts/MainFunctionNavbar';
+import UnitSpaceInfoRow from '../../components/ui/UnitSpaceInfoRow';
+
+export default function LocationList() {
+  return (
+    <>
+      <MainFunctionNavbar />
+      <UnitSpaceInfoRow />
+    </>
+  );
+}
