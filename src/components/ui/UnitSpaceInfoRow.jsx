d2a2891e5c8f184526b7a268592c6d4c7bbfb5e6
Seoyeon Park <qkrtj418@gmail.com>
2024-01-21 12:38:21 +0900

    Feat: 대관정보 목록 페이지 레이아웃

--- src/components/ui/UnitSpaceInfoRow.jsx
+++ src/components/ui/UnitSpaceInfoRow.jsx
@@ -1,9 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import * as S from './UnitSpaceInfoRow.style';
+import Button70 from '../atoms/Button70';
 
 export default function UnitSpaceInfoRow() {
+  const navigate = useNavigate();
+  const goMatchApply = () => {
+    navigate('/home');
+  };
+
   return (
     <S.SpaceInfoItemContainer>
-      <S.SpaceName>체육관명</S.SpaceName>
+      <S.SpaceTime>15:00</S.SpaceTime>
+      <S.SpaceInfo>
+        <S.SpaceName>체육관명</S.SpaceName>
+        <S.MatchingInfo>남자|18명|3파전|중 레벨</S.MatchingInfo>
+      </S.SpaceInfo>
+      <S.ButtonContainer>
+        <Button70 onClick={goMatchApply}>1명 모집</Button70>
+      </S.ButtonContainer>
     </S.SpaceInfoItemContainer>
   );
 }
