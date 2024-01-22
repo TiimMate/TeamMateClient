cbd0fe41b5de810d3bb36ffad9555b643af19961
misung.dev <misung.dev@gmail.com>
2024-01-23 00:20:29 +0900

    Feat: 홈페이지 내 이미지 파일 및 일부 코드 업데이트

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import * as S from './HomePage.style';
+import '../../assets/img-ball';
+
+const images = [
+  'img-ball-basketball.png',
+  'img-ball-baseball.png',
+  'img-ball-tennis.png',
+  'img-ball-soccer.png',
+  'img-ball-futsal.png',
+  'img-ball-volleyball.png',
+  'img-ball-bowling.png',
+  'img-ball-badminton.png',
+  'img-ball-pingpong.png',
+];
 
 export default function HomePage() {
   return (
@@ -10,10 +23,25 @@ export default function HomePage() {
           <br />
           다양한 종목을 만나보세요
         </S.MainTitle>
-        <S.MainImage>s</S.MainImage>
+        <S.MainImage className='grid-container'>
+          <S.BallContainer>
+            <S.BallImage>
+              {images.map((image, index) => (
+                <div key={index} className='grid-item'>
+                  <img src={image} alt={`Image ${index + 1}`} />
+                </div>
+              ))}
+            </S.BallImage>
+            <S.BallName>농구</S.BallName>
+          </S.BallContainer>
+        </S.MainImage>
         <S.ImageInfo>클릭하면 각 종목 페이지로 이동합니다.</S.ImageInfo>
       </S.Container>
-      <S.ServiceInfo>팀메이트는 이런 서비스예요</S.ServiceInfo>
+      <S.ServiceInfo>
+        팀메이트는 이런 서비스예요
+        <br />
+        (업데이트 예정)
+      </S.ServiceInfo>
     </S.PageLayout>
   );
 }
