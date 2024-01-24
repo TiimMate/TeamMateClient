fdb1e0b0d0fe6b3a8db7c57876c6bb11ab393f36
Seoyeon Park <qkrtj418@gmail.com>
2024-01-24 11:07:47 +0900

    Feat: 수정된 디자인 반영

--- src/pages/location/LocationList.jsx
+++ src/pages/location/LocationList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import NewPost from '../../components/layouts/NewPost';
 import MainFunctionNavbar from '../../components/layouts/MainFunctionNavbar';
+import LocationListBar from '../../components/layouts/LocationListBar';
 import UnitSpaceInfoRow from '../../components/ui/UnitSpaceInfoRow';
 
 export default function LocationList() {
   return (
     <>
       <MainFunctionNavbar />
+      <NewPost />
+      <LocationListBar />
       <UnitSpaceInfoRow />
     </>
   );
