a35a027fff55a955502e10ab53b5bf778aeb608a
misung.dev <misung.dev@gmail.com>
2024-01-29 03:11:16 +0900

    Feat: 리뷰 작성 페이지 내 모달 창 구현

--- src/components/ui/ReviewModal/ReviewModal.jsx
+++ src/components/ui/ReviewModal/ReviewModal.jsx
@@ -1,21 +1,25 @@
+import { useState } from 'react';
 import { PORTAL_KEYS } from '../../../constants/portalKeys';
 import Portal from '../../layouts/Portal';
 import Modal from '../Modal/Modal';
+import Rating from '../Rating';
 import * as S from './ReviewModal.style';
 
 const ReviewModal = ({ isOpen, onClose }) => {
+  const [guestScore, setGuestScore] = useState(0);
+  const [sportsmanshipScore, setSportsmanshipScore] = useState(0);
+
+  const handleGuestRatingChange = (score) => {
+    setGuestScore(score);
+  };
+
+  const handleSportsmanshipRatingChange = (score) => {
+    setSportsmanshipScore(score);
+  };
+
   if (!isOpen) {
     return null;
   }
-
-  const REVIEW_TITLE_LIST = [
-    {
-      title: '팀 실력은 어떤가요?',
-    },
-    {
-      title: '스포츠맨쉽을 보여줬나요?',
-    },
-  ];
 
   return (
     <Portal portalKey={PORTAL_KEYS.modalLayout}>
@@ -25,16 +29,19 @@ const ReviewModal = ({ isOpen, onClose }) => {
             매치는 즐거우셨나요?
             <br />더 나은 매치를 위해 의견을 들려주세요!
           </S.ReviewDescription>
-          <S.ReviewContainer>
-            <S.ReviewContent>
-              {REVIEW_TITLE_LIST.map(({ title }) => (
-                <S.ReviewTitle key={title}>{title}</S.ReviewTitle>
-              ))}
-              <S.ReviewCheckbox>
-                <S.ReviewCheck>e</S.ReviewCheck>
-              </S.ReviewCheckbox>
-            </S.ReviewContent>
-          </S.ReviewContainer>
+
+          <S.RatingGroup>
+            <Rating
+              label='팀의 실력은 어떤가요?'
+              score={guestScore}
+              onChange={handleGuestRatingChange}
+            />
+            <Rating
+              label='스포츠맨쉽을 보여줬나요?'
+              score={sportsmanshipScore}
+              onChange={handleSportsmanshipRatingChange}
+            />
+          </S.RatingGroup>
         </S.ReviewLayout>
       </Modal>
     </Portal>
--- src/components/ui/ReviewModal/ReviewModal.style.jsx
+++ src/components/ui/ReviewModal/ReviewModal.style.jsx
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 export const ReviewLayout = styled.div`
   display: flex;
   flex-direction: column;
+  margin-bottom: 10px;
   width: 100%;
   gap: 14px;
 `;
 
-// 순서 정렬
 export const ReviewDescription = styled.div`
   line-height: 18px;
   letter-spacing: -0.6px;
@@ -16,30 +16,8 @@ export const ReviewDescription = styled.div`
   color: var(--Gray300, #636363);
 `;
 
-export const ReviewContainer = styled.div`
-  gap: 10px;
+export const RatingGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
-
-export const ReviewContent = styled.div`
-  gap: 8px;
-`;
-
-export const ReviewTitle = styled.h2`
-  line-height: 29px;
-  letter-spacing: -0.8px;
-  text-align: center;
-  font-size: 16px;
-  font-weight: 600;
-  color: var(--Blue300, #0075ff);
-`;
-
-export const ReviewCheckbox = styled.div`
-  display: inline-flex;
-  align-items: flex-start;
-  gap: 12px;
-  border-radius: 6px;
-  background: var(--Blue100, #f4f9ff);
-  padding: 10px 30px;
-`;
-
-export const ReviewCheck = styled.div``;
