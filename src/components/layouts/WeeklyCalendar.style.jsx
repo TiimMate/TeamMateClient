50d6a8b99433d63ea7847c81b8ee643b54a069d5
misung.dev <misung.dev@gmail.com>
2024-01-30 15:56:39 +0900

    Style: 주간 달력 border-bottom 추가

--- src/components/layouts/WeeklyCalendar.style.jsx
+++ src/components/layouts/WeeklyCalendar.style.jsx
@@ -1,26 +1,14 @@
 import styled from 'styled-components';
 
-export const P = styled.p`
-  width: fit-content;
-  padding-top: 10px;
-  padding-right: 5px;
-  padding-bottom: 10px;
-  padding-left: 5px;
+export const Main = styled.div`
+  width: 100%;
+`;
 
-  border-radius: 6px;
-
-  background: ${(props) => props.back};
-
-  font-family: 'Pretendard Variable';
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 500; /* 글자가 너무 두꺼워보여서 700 -> 500으로 수정했어요*/
-
-  line-height: 18px; /* 150% */
-
-  color: ${(props) => props.color || 'black'};
-
-  white-space: pre-line;
+export const Wrapper = styled.nav`
+  overflow: hidden;
+  border-bottom: 1px solid var(--gray-200, #d9d9d9);
+  height: 70px;
+  background: var(--White, #fff);
 `;
 
 export const Button = styled.div`
@@ -29,23 +17,25 @@ export const Button = styled.div`
   justify-content: center;
   flex-direction: column;
   float: left;
-
   width: 14.2%;
   height: 70px;
-
   background: ${(props) => props.back};
-
   color: ${(props) => props.color || 'black'};
 `;
 
-export const Main = styled.div`
-  width: 100%;
-`;
-
-export const Wrapper = styled.nav`
-  overflow: hidden;
-
-  height: 70px;
-
-  background: var(--White, #fff);
+export const P = styled.p`
+  width: fit-content;
+  padding-top: 10px;
+  padding-right: 5px;
+  padding-bottom: 10px;
+  padding-left: 5px;
+  border-radius: 6px;
+  background: ${(props) => props.back};
+  font-family: 'Pretendard Variable';
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500; /* 글자가 너무 두꺼워보여서 700 -> 500으로 수정했어요*/
+  line-height: 18px; /* 150% */
+  color: ${(props) => props.color || 'black'};
+  white-space: pre-line;
 `;
