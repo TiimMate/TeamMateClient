9a295031dd48534d6f7d0aa2a08fcefa0b3b0b2a
daol <gsj990916@hanyang.ac.kr>
2024-02-01 02:15:51 +0900

    Feat: HeightSelector 구현
    - 신장을 입력하는 selector을 구현했습니다

--- src/components/layouts/Selector/Height/HeightSelector.jsx
+++ src/components/layouts/Selector/Height/HeightSelector.jsx
@@ -1,13 +1,68 @@
 import styled from 'styled-components';
-import chevronDown4 from '../../../../assets/chevron-down-4 1.svg';
+import { useEffect, useRef, useState } from 'react';
 
-function HeightSelector() {
+function HeightSelector({ height }) {
+  const [isActive, setIsActive] = useState(false);
+  const [numbers, setNumbers] = useState(
+    height ? height.split('') : [null, null, null],
+  );
+  const [numOfInputs, setNumOfInputs] = useState(0);
+
+  const btnRef = useRef();
+
+  const onClickBtn = () => {
+    setNumbers([null, null, null]);
+    setNumOfInputs(0);
+    setIsActive(true);
+  };
+
+  // 입력시 키 감지
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (!isActive) return;
+      if (!/^\d$/.test(event.key)) return; // 숫자가 아닐시
+
+      // 숫자 업데이트
+      const newNumbers = [...numbers];
+      newNumbers[numOfInputs] = event.key;
+      setNumbers(newNumbers);
+      setNumOfInputs(numOfInputs + 1);
+    };
+
+    if (isActive) {
+      window.addEventListener('keydown', handleKeyPress);
+    } else {
+      window.removeEventListener('keydown', handleKeyPress);
+    }
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [isActive, numbers, numOfInputs]);
+
+  // 버튼 밖 클릭시
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (btnRef.current && !btnRef.current.contains(event.target)) {
+        setIsActive(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <>
-      <Label>신장*</Label>
-      <ButtonWithChevron>
-        지역을 선택해주세요 <img src={chevronDown4} alt='chevron' />
-      </ButtonWithChevron>
+      <Label>신장</Label>
+      <Button ref={btnRef} $isActive={isActive} onClick={onClickBtn}>
+        <Number $isReceived={numbers[0]}>{numbers[0] || 1}</Number>
+        <Number $isReceived={numbers[1]}>{numbers[1] || 7}</Number>
+        <Number $isReceived={numbers[2]}>{numbers[2] || 0}</Number>
+        cm
+      </Button>
     </>
   );
 }
@@ -26,7 +81,7 @@ const Label = styled.label`
   margin-bottom: 0.31rem;
 `;
 
-export const ButtonWithChevron = styled.button`
+export const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -39,17 +94,33 @@ export const ButtonWithChevron = styled.button`
   margin-bottom: 1.25rem;
   padding: 0.625rem;
   border-radius: 0.375rem;
-  border: 1px solid var(--Gray200, #d9d9d9);
+  border: 1px solid
+    ${(props) => (props.$isActive ? '#0075ff' : 'var(--Gray200, #d9d9d9)')};
   background: #fff;
   cursor: pointer;
 
   color: var(--Gray300, #636363);
   font-family: Pretendard;
-  font-size: 0.75rem;
+  font-size: 1.875rem;
   font-style: normal;
-  font-weight: 500;
-  line-height: 150%; /* 1.125rem */
-  letter-spacing: -0.0375rem;
+  font-weight: 400;
+  line-height: 1.875rem; /* 100% */
+  letter-spacing: -0.09375rem;
+
+  &:focus {
+    border: 1px solid #0075ff;
+  }
+`;
+
+const Number = styled.b`
+  color: ${(props) =>
+    props.$isReceived ? 'var(--Gray300, #636363)' : 'var(--Gray200, #d9d9d9)'};
+  font-family: Pretendard;
+  font-size: 1.875rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 2.625rem; /* 140% */
+  letter-spacing: -0.09375rem;
 `;
 
 export default HeightSelector;
