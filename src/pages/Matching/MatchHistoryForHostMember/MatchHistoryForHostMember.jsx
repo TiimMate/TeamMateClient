ab85cb6612ca5f85a0934afc9ca16d7f3ed9b631
misung.dev <misung.dev@gmail.com>
2024-02-01 03:21:52 +0900

    Feat: 팀원 목록 페이지 생성

---
+++ src/pages/Matching/MatchHistoryForHostMember/MatchHistoryForHostMember.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import * as S from './MatchHistoryForHostMember.style';
+import WeeklyCalendar from '../../../components/layouts/WeeklyCalendar';
+// import MatchList from '../../../components/layouts/Matching/MatchMemberList';
+import MatchList from '../../../components/layouts/Matching/MatchList';
+
+const NAV_ITEM_LIST = [
+  {
+    label: '게스팅 내역',
+    url: '/matching/history/',
+    isActive: false,
+  },
+  {
+    label: '호스팅 내역',
+    url: '/matching/history/host',
+    isActive: true,
+  },
+];
+
+export default function MatchHistoryForHostMember() {
+  return (
+    <S.PageLayout>
+      <S.Nav>
+        {NAV_ITEM_LIST.map(({ label, url, isActive }) => (
+          <S.NavItem key={label} href={url} isActive={isActive}>
+            {label}
+          </S.NavItem>
+        ))}
+      </S.Nav>
+      <S.Banner></S.Banner>
+      <WeeklyCalendar />
+      <MatchList />
+    </S.PageLayout>
+  );
+}
