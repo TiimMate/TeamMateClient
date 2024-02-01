e98c63de0669c302cd5d719d7de03911411ce621
misung.dev <misung.dev@gmail.com>
2024-02-01 13:22:12 +0900

    Feat: 멤버 리스트 컴포넌트 생성(일부)

---
+++ src/components/layouts/Matching/MatchMemberList.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as S from './MatchMemberList.style';
+import useModal from '../../../hooks/useModal';
+import ReviewModal from '../../ui/ReviewModal';
+import {
+  MEMBER_RAW_DATA_BASKETBALL,
+  formatMemberData,
+} from '../../../utils/formatData';
+
+function MatchMemberList({ name }) {
+  const members = formatMemberData(MEMBER_RAW_DATA_BASKETBALL);
+
+  const { height, weight, position } = members[0].unitInfo;
+
+  const { isOpen, openModal, closeModal } = useModal();
+
+  return (
+    <S.Container>
+      <S.ProfileImg
+        src='../../../assets/svg-profile-img-sample.svg'
+        alt={name}
+      />
+
+      <S.Space />
+      <S.Content>
+        <S.TeamName>{name}</S.TeamName>
+        <S.MatchDetail>
+          {`${height}cm | ${weight}kg | ${position}`}
+        </S.MatchDetail>
+      </S.Content>
+      <S.Space />
+      <S.Button onClick={openModal}>리뷰 작성</S.Button>
+      <ReviewModal isOpen={isOpen} onClose={closeModal} member={members[0]} />
+    </S.Container>
+  );
+}
+
+export default MatchMemberList;
