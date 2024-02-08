0d80dbe57d134824c8aa2a413a1e778a7c98b405
misung.dev <misung.dev@gmail.com>
2024-02-08 22:42:46 +0900

    Feat: 홈페이지 내 서비스소개 이미지 추가

--- src/components/layouts/Footer.style.jsx
+++ src/components/layouts/Footer.style.jsx
@@ -5,6 +5,7 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  background-color: #f5f5f5;
   width: 100%;
   height: 126px;
   gap: 13px;
