8f024b7f83f13750e1124fad858a7c87737d4959
Seoyeon Park <qkrtj418@gmail.com>
2024-02-16 15:24:36 +0900

    Design: 목록에서 구분 항목 제거

--- src/components/ui/UnitBoardRow/UnitBoardRow.jsx
+++ src/components/ui/UnitBoardRow/UnitBoardRow.jsx
@@ -3,19 +3,19 @@ import notice from '../../../assets/icon_notice.svg';
 import yesBookmark from '../../../assets/icon_yes_bookmark.svg';
 import noBookmark from '../../../assets/icon_no_bookmark.svg';
 
-export default function UnitBoardRow({ id, unitBoard }) {
-  const { category, title, date } = unitBoard;
+export default function UnitBoardRow({ id, category, title, date }) {
+  // const renderIcon = (category) => {
+  //   if (category === 'notice') return <img src={notice} alt='notice' />;
+  //   if (category === 'bookmark')
+  //     return <img src={yesBookmark} alt='bookmark' />;
+  //   if (category === '') return <img src={noBookmark} alt='' />;
 
-  const renderIcon = (category) => {
-    if (category === 'notice') return <img src={notice} alt='notice' />;
-    if (category === 'bookmark')
-      return <img src={yesBookmark} alt='bookmark' />;
-    if (category === '') return <img src={noBookmark} alt='' />;
-  };
+  //   if (category === true) return <img src={yesBookmark} alt='bookmark' />;
+  //   if (category === false) return <img src={noBookmark} alt='' />;
+  // };
 
   return (
     <S.Wrapper>
-      <S.Icon>{renderIcon(category)}</S.Icon>
       <S.Title to={`/community/${id}/detail`}>{title}</S.Title>
       <S.Date>{date}</S.Date>
     </S.Wrapper>
