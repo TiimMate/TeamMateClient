1521cfd37052cb6df24f3fa9c3c326a6c22b5f8a
misung.dev <misung.dev@gmail.com>
2024-02-20 03:50:21 +0900

    Feat: 배너 이미지 변경

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import teammateBanner from '../../assets/banner-teammate.png';
 
 export default function Banner() {
   return <BannerSection />;
 }
 
 const BannerSection = styled.div`
-  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 160px;
-
-  background: var(--Blue400, #2e69ff);
+  background-image: url(${teammateBanner});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
 `;
