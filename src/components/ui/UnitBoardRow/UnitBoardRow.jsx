9d10d7ea9e7f7adf72837bf820e60ad1132eb36b
Seoyeon Park <qkrtj418@gmail.com>
2024-02-19 17:28:08 +0900

    Feat: 작성일시 출력형식 변환

--- src/components/ui/UnitBoardRow/UnitBoardRow.jsx
+++ src/components/ui/UnitBoardRow/UnitBoardRow.jsx
@@ -3,6 +3,7 @@ import * as S from './UnitBoardRow.style';
 import notice from '../../../assets/icon_notice.svg';
 import yesBookmark from '../../../assets/icon_yes_bookmark.svg';
 import noBookmark from '../../../assets/icon_no_bookmark.svg';
+import { Unix_timestampConv, timeStamp } from '../../../utils/timeStamp';
 
 const UnitBoardRow = forwardRef(({ id, category, title, date }, ref) => {
   const renderIcon = (category) => {
@@ -10,11 +11,14 @@ const UnitBoardRow = forwardRef(({ id, category, title, date }, ref) => {
     if (category === false) return <img src={noBookmark} alt='' />;
   };
 
+  const tempDate = new Date(date);
+  const printDate = timeStamp(Unix_timestampConv(tempDate.getTime()));
+
   return (
     <S.Wrapper ref={ref}>
       {renderIcon(category)}
       <S.Title to={`/community/${id}/detail`}>{title}</S.Title>
-      <S.Date>{date}</S.Date>
+      <S.Date>{printDate}</S.Date>
     </S.Wrapper>
   );
 });
